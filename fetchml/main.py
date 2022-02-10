bca10309069872ec18ac59cc6044936bc45639e7
widkit <exylloath@gmail.com>
2022-02-11 00:38:22 +0300

    replacing all variables successfully

--- fetchml/main.py
+++ fetchml/main.py
@@ -1,4 +1,17 @@
-os = 'macos'
+os = ''
+host = ''
+kernel = ''
+uptime = ''
+packages = ''
+shell = ''
+resolution = ''
+terminal = ''
+cpu = ''
+gpu = ''
+memory = ''
+desktop_enviroment = ''
+window_manager = ''
+wmtheme = ''
 
 # Reading the file
 with open('index.html', 'r') as file:
@@ -6,7 +19,22 @@ with open('index.html', 'r') as file:
 
     # Doing the replacements of variables
     filedata = filedata.replace('$os', '{}').format(os)
+    filedata = filedata.replace('$host', '{}').format(host)
+    filedata = filedata.replace('$kernel', '{}').format(kernel)
+    filedata = filedata.replace('$uptime', '{}').format(uptime)
+    filedata = filedata.replace('$packages', '{}').format(packages)
+    filedata = filedata.replace('$shell', '{}').format(shell)
+    filedata = filedata.replace('$resolution', '{}').format(resolution)
+    filedata = filedata.replace(
+        '$desktop_enviroment', '{}').format(desktop_enviroment)
+    filedata = filedata.replace('$window_manager', '{}').format(window_manager)
+    filedata = filedata.replace(
+        '$wmtheme', '{}').format(wmtheme)
+    filedata = filedata.replace('$terminal', '{}').format(terminal)
+    filedata = filedata.replace('$cpu', '{}').format(cpu)
+    filedata = filedata.replace('$gpu', '{}').format(gpu)
+    filedata = filedata.replace('$memory', '{}').format(memory)
 
-# Writing changes to new file
-with open('render.html', 'w') as file:
-    file.write(filedata)
+    # Writing changes to new file
+    with open('render.html', 'w') as file:
+        file.write(filedata)
